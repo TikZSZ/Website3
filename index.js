6061d76684dd95481f0fcda9d300efd59c9ce8d7
TikZSZ <75272420+TikZSZ@users.noreply.github.com>
2021-01-09 11:43:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,82 +1,230 @@
+var span = document.querySelector('.span').innerHTML;
 // style functions
-function addstyle(p){
-    p.add("pressed")
-    p.remove("pink")
-    p.add("red")
-}
+function addstyle(p) {
+	p.classList.add("pressed");
+	p.classList.remove("pink");
+	p.classList.add("red");
+};
+
 function removestyle(p) {
-    setTimeout(function () {
-        p.remove("red");
-        p.add("pink");
-        p.remove("pressed");
-    }, 100);
-}
-
-
+	setTimeout(function () {
+		p.classList.remove("red");
+		p.classList.add("pink");
+		p.classList.remove("pressed");
+	}, 100);
+};
 // event listners
-    // touch listner
-const btnArray = document.querySelectorAll('.drum')
-for(var i=0;i<btnArray.length;i++){
-    btnArray[i].addEventListener('click', function(){
-        play(this.innerHTML)
-    })
-}
-    // press listner
-document.addEventListener('keydown',function(event){
-    console.log(event.key);
-    play(event.key)
-})
-
-
-
+// touch listner
+const btnArray = document.querySelectorAll('.drum');
+for (var i = 0; i < btnArray.length; i++) {
+	btnArray[i].addEventListener('click', function () {
+		document.querySelector('.input').innerHTML += this.innerHTML;
+		play(this.innerHTML);
+	});
+};
+// press listner
+document.addEventListener('keydown', function (event) {
+	document.querySelector('.input').innerHTML += event.key;
+	play(event.key);
+});
 // player
-function play(keyp){
-    var audio = new Audio();
-        switch (keyp) {
-            case "w":
-                addstyle(document.querySelector('.crash').classList)
-                audio.src="sounds/crash.mp3"
-                audio.play()
-                removestyle(document.querySelector('.crash').classList)
-                break;
-            case "a":
-                addstyle(document.querySelector('.kick-bass').classList)
-                audio.src="sounds/kick-bass.mp3"
-                audio.play()
-                removestyle(document.querySelector('.kick-bass').classList)
-                break;
-            case "s":
-                addstyle(document.querySelector('.snare').classList)
-                audio.src="sounds/snare.mp3"
-                audio.play()
-                removestyle(document.querySelector('.snare').classList)
-                break;
-            case "d":
-                addstyle(document.querySelector('.tom-1').classList)
-                audio.src="sounds/tom-1.mp3"
-                audio.play()
-                removestyle(document.querySelector('.tom-1').classList)
-                break;
-            case "j":
-                addstyle(document.querySelector('.tom-2').classList)
-                audio.src="sounds/tom-2.mp3"
-                audio.play()
-                removestyle(document.querySelector('.tom-2').classList)
-                break;
-            case "k":
-                addstyle(document.querySelector('.tom-3').classList)
-                audio.src="sounds/tom-3.mp3"
-                audio.play()
-                removestyle(document.querySelector('.tom-3').classList)
-                break;
-            case "l":
-                addstyle(document.querySelector('.tom-4').classList)
-                audio.src="sounds/tom-4.mp3"
-                audio.play()
-                removestyle(document.querySelector('.tom-4').classList)
-                break;
-            default:
-                break;
-        }
-}
+var audio1 = new Audio();
+audio1.src = "sounds/crash.mp3";
+var audio2 = new Audio();
+audio2.src = "sounds/kick-bass.mp3";
+var audio3 = new Audio();
+audio3.src = "sounds/snare.mp3";
+var audio4 = new Audio();
+audio4.src = "sounds/tom-1.mp3";
+var audio5 = new Audio();
+audio5.src = "sounds/tom-2.mp3";
+var audio6 = new Audio();
+audio6.src = "sounds/tom-3.mp3";
+var audio7 = new Audio();
+audio7.src = "sounds/tom-4.mp3";
 
+function play(keyp) {
+
+	switch (keyp) {
+	case "w":
+		var w = document.querySelector('.crash');
+		addstyle(w);
+		tutroial(w);
+		audio1.play();
+		removestyle(w);
+		break;
+	case "a":
+		var a = document.querySelector('.kick-bass');
+		addstyle(a);
+		tutroial(a);
+		audio2.play();
+		removestyle(a);
+		break;
+	case "s":
+		var s = document.querySelector('.snare');
+		addstyle(s);
+		tutroial(s);
+		audio3.play();
+		removestyle(s);
+		break;
+	case "d":
+		var d = document.querySelector('.tom-1');
+		addstyle(d);
+		tutroial(d);
+		audio4.play();
+		removestyle(d);
+		break;
+	case "j":
+		var j = document.querySelector('.tom-2');
+		addstyle(j);
+		tutroial(j);
+		audio5.play();
+		removestyle(j);
+		break;
+	case "k":
+		var k = document.querySelector('.tom-3');
+		addstyle(k);
+		tutroial(k);
+		audio6.play();
+		removestyle(k);
+		break;
+	case "l":
+		var l = document.querySelector('.tom-4');
+		addstyle(l);
+		tutroial(l);
+		audio7.play();
+		removestyle(l);
+		break;
+	default:
+		break;
+	}
+}
+// tutorial
+// variables related to tutorial
+var count = 0;
+var str = "jkjkjkjkkwdjdjdjdjdjw";
+var status = "false";
+var style = document.querySelectorAll('.style');
+// functions related to tutorials
+function tutroial(p) {
+	if (status === "true") {
+		if (p.innerHTML === str[count]) {
+			count = count + 1;
+			document.querySelector('.style').innerHTML = span.slice(0, count) + `<span class="span">${span.slice(count,span.length)}</span>`;
+			document.querySelector('.user').innerHTML += p.innerHTML;
+                        if((count)===str.length){
+    document.getElementById('tutorial').click()
+    document.getElementById('tutorial').click()
+                        };
+		};
+	};
+};
+
+function tutorialclick() {
+	if (status === "false") {
+		style[0].classList.remove("hidden");
+		style[1].classList.remove("hidden");
+		document.querySelector('#tutorial').innerHTML = "Stop Tutorial";
+		status = "true";
+	} else if (status === "true") {
+		style[0].classList.add("hidden");
+		style[1].classList.add("hidden");
+		style[1].innerHTML = ":";
+		document.querySelector('#tutorial').innerHTML = "Start Tutorial"
+        document.querySelector('.users').innerHTML=`<span class="span">${span}</span>`
+          	count = 0;
+		status = "false";
+	};
+}
+var is_history = "false"
+
+function show() {
+	if (is_history === "false") {
+		document.querySelector('.div').classList.remove('hidden');
+		is_history = "true";
+		document.querySelector('#history').innerHTML = "Hide My History";
+	} else if (is_history = "true") {
+		document.querySelector('.div').classList.add('hidden');
+		is_history = "false";
+		document.querySelector('#history').innerHTML = "Show My History";
+	};
+};
+
+var scroll_status=false;
+function analyse(){
+	console.log(scroll_status);
+	if(!scroll_status){
+		scroll_status=true;
+		document.getElementById('analyse').innerHTML="Stop Long History";
+	}else{
+		scroll_status=false;
+		document.getElementById('analyse').innerHTML="Long History";
+
+	};
+	
+};
+
+// function for scroll synchronization
+
+var TopDiv = document.querySelector('.users');
+var BottomDiv = document.querySelector('.user');
+TopDiv.onscroll = function () {
+	BottomDiv.scrollLeft = this.scrollLeft;
+};
+BottomDiv.onscroll = function () {
+	TopDiv.scrollLeft = this.scrollLeft;
+	
+
+};
+
+var el1 = document.querySelector('.user');
+var el2 = document.querySelector('.input');
+
+
+setInterval(autoScroll, 100);
+
+function autoScroll() {
+	// check if scrolled to right
+	var scroll = checkScrolledToRight(el1);
+	var scroll2 = checkScrolledToRight(el2);
+	
+		
+	// if not scroll to right
+	if (!scroll) {
+		scrollToRight(el1);		
+	};
+	if(!scroll_status){
+		if (!scroll2) {
+			scrollToRight(el2);		
+		};
+	}
+
+};
+
+function checkScrolledToRight(el) {
+	if(el.scrollLeft >= (el.scrollWidth - el.offsetWidth)){
+		return true;
+	} else {
+		return false;
+	};
+};
+
+function scrollToRight(el) {
+	el.scrollLeft = (el.scrollWidth);
+};
+
+
+// function scrolldown() {
+// 	setTimeout(
+// 	  function()
+// 	  {
+// 		el1.scrollLeft=(el1.scrollWidth);
+// 		console.log("hello 2");
+// 		scrolldown();
+// 	  }, 100
+// 	)
+//   }
+
+// scrolldown()
+  
+ 
